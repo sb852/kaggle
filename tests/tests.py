367d272f91a0ad99636c0c2b7510a01143d1084d
drawer87 <u3003097@hku.hk>
2017-12-23 18:16:11 +0100

    added more tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+import numpy as np
 src_path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/src'
 sys.path.append(src_path)
 
@@ -40,7 +41,33 @@ class TestAdd(unittest.TestCase):
         perform_preprocessing = True
         train_x, train_y, test_x = preprocess_save_data(perform_preprocessing)
 
-        print('x')
+        #  We expect to have three label colunms and 10866 observations.
+        self.assertEqual(train_y.shape[1], 3)
+        self.assertEqual(train_y.shape[0], 10886)
+
+        self.assertEqual(train_x.shape[0], 10886)
+
+        #  We expect the following columns to be present.
+        present_columns = ['hour', 'day_of_year', 'day_of_month', 'day_of_week', 'day_off', 'day',
+                           'is_weekend', 'hours_categorized_2']
+
+        test = [column_name in train_x.columns for column_name in present_columns]
+        self.assertEqual(np.unique(test), True)
+
+
+    def test_produce_random_parameters(self):
+        """
+        We are testing the production of randomized hyperparameters.
+        """
+        from training import produce_random_parameters
+
+        #  We expect the following columns to be present.
+        columns_expected = ['max_depth', 'n_estimators', 'learning_rate', 'subsample', 'colsample_by_level',
+                            'min_child_weight', 'min_child_weight', 'early_stopping', 'size_xgb_val']
+        parameters = produce_random_parameters()
+
+        test = [column_name in parameters.keys() for column_name in columns_expected]
+        self.assertEqual(np.unique(test), True)
 
 
 
